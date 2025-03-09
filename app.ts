7100dd623f0c2dbd3080e327d211af1b7c2dfe24
ankit0590 <ankitkurve001@gmail.com>
2025-03-09 15:36:48 +0530

    function name is updated

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import {verifypan,verifyadhar} from './src/index'
+import {verifyPan,verifyAdhar} from './src/index'
 
 const app=express();
 
 app.use(express.json());
 
-app.post('/verifypan',verifypan);
-app.post('/verfyadhar',verifyadhar)
+app.post('/verifypan',verifyPan);
+app.post('/verfyadhar',verifyAdhar)
 
 app.listen(3000,()=>{
         console.log(`System is Running on ${3000}`);
